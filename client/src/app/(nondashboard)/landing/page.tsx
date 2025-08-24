65842ac9fb8defa8f764336375ef06960072b34a
bullseye4703 <atlixdowell3@gmail.com>
2025-08-25 02:15:15 +0530

    first commit - auth & settings completed

---
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Herosection from './HeroSection'
+import FeaturesSection from './FeaturesSection'
+import DiscoverSection from './DiscoverSection'
+import CallToActionSection from './CallToActionSection'
+import FooterSection from './FooterSection'
+const Landing = () => {
+  return (
+    <div><Herosection />
+      <FeaturesSection/>
+      <DiscoverSection/>
+      <CallToActionSection/>
+      <FooterSection/>
+    </div>
+  )
+}
+
+export default Landing;
---
+++ server/src/index.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { authMiddleware } from './middleware/authMiddleware';
+import tenantRoutes from './routes/tenantRoutes';
+import managerRoutes from './routes/managerRoutes';
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.use('/tenants', authMiddleware(['tenant']), tenantRoutes);
+app.use('/managers', authMiddleware(['manager']), managerRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
